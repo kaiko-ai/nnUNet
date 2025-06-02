d3490dfa3e8ef592d84b21c39e5aa4b9e230641a
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-06-02 14:30:10 +0200

    fixed overrides

--- nnunetv2/training/nnUNetTrainer/variants/fm/nnUNetTrainerFM.py
+++ nnunetv2/training/nnUNetTrainer/variants/fm/nnUNetTrainerFM.py
@@ -12,8 +12,8 @@ class nnUNetTrainerFM(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, device=None):
         super().__init__(plans, configuration, fold, dataset_json, device)
 
-        self.configuration_manager.network_arch_class_name = "voco_b"
-        self.configuration_manager.patch_size = [96, 96, 96]
+        self.configuration_manager.configuration['architecture']['network_class_name'] = "voco_b"
+        self.configuration_manager.configuration['patch_size'] = [96, 96, 96]
 
     @override
     def build_network_architecture(self, architecture_class_name: str,
